1560e7f5eddd5045a1f660e76cf149afc7e51277
Avenger2627 <imraninfo97@gmail.com>
2020-12-19 18:49:12 +0530

    [Imran Shaikh] Add: Edit Contact with First Name

--- AddressBook/AddressBook.cs
+++ AddressBook/AddressBook.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace AddressBook
 {
+    /// <summary>
+    /// U1. Create Contact Person List.
+    /// </summary>
     class AddressBok
     {
         List<PersonalDetail> DetailList;
@@ -11,7 +14,9 @@ namespace AddressBook
         {
             this.DetailList = new List<PersonalDetail>();
         }
-
+        /// <summary>
+        /// U2. Add Contact in Contact Book
+        /// </summary>
         public void AddContact()
         {
             Console.Write("Enter First Name: ");
@@ -28,7 +33,40 @@ namespace AddressBook
             string PhoneNumber = Console.ReadLine();
             PersonalDetail Person = new PersonalDetail(FirstName, LastName, City, State, Zip, PhoneNumber);
             DetailList.Add(Person);
-            Person.Display();
+        }
+
+        /// <summary>
+        ///U3. Edits the contact.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        public  void EditContact(string fname)
+        {
+            int size = DetailList.Count;
+            int check = 0;
+            foreach (PersonalDetail Contact in DetailList)
+            {
+                check++;
+                if (Contact.firstName.Equals(fname))
+                {
+                   AddContact();
+                    DetailList.Remove(Contact);
+                    Console.WriteLine("Contact Updated Successfully...");
+                    break;
+                }
+                else if (size == check)
+                {
+                    Console.WriteLine(fname + " not found in addressbook...");
+                    break;
+                }
+            }
+        }
+
+        public void DisplayContact()
+        {
+            foreach (PersonalDetail Contact in DetailList)
+            {
+                Console.WriteLine("\nFirstName: " + Contact.firstName + "\nLastName: " + Contact.lastName + "\nCity: " + Contact.city + "\nState: " + Contact.state + "\nZip Code: " + Contact.zip + "\nMobileNumber: " + Contact.phoneNumber);
+            }
         }
     }
 }
--- AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBookMain.cs
@@ -8,16 +8,27 @@ namespace AddressBook
         {
             Console.WriteLine("*********Welcome To Address Book*********");
             AddressBok Book = new AddressBok();
-            Console.Write("Enter Choice To Proceed\n1.Add Contact\n2.Exit");
-            int Choice = Convert.ToInt32(Console.ReadLine());
-            switch (Choice)
+            while (true)
             {
-                case 1:
-                    Book.AddContact();
-                    break;
-                case 2:
-                    Environment.Exit(0);
-                    break;
+                Console.WriteLine("Enter Choice To Proceed\n1.Add Contact\n2.EditContact\n3.Display\n0.Exit");
+                int Choice = Convert.ToInt32(Console.ReadLine());
+                switch (Choice)
+                {
+                    case 1:
+                        Book.AddContact();
+                        break;
+                    case 2:
+                        Console.Write("Enter FirstName To Edit: ");
+                        string Name = Console.ReadLine();
+                        Book.EditContact(Name);
+                        break;
+                    case 3:
+                        Book.DisplayContact();
+                        break;
+                    case 0:
+                        Environment.Exit(0);
+                        break;
+                }
             }
         }
     }
