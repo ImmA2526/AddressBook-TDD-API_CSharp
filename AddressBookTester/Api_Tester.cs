b8cd10b2daf8701327733b54cbced7ce625edaf5
Imran2526 <imraninfo97@gmail.com>
2021-01-07 22:39:02 +0530

    [Imran] Add: Ability to Add Multiple Contact

--- AddressBookTester/Api_Tester.cs
+++ AddressBookTester/Api_Tester.cs
@@ -57,5 +57,49 @@ namespace AddressBookTester
                 Console.WriteLine("\nFirstName: " + e.firstName + "\nLastName: " + e.lastName + "\nCity: " + e.city + "\nState: " + e.state + "\nZip Code: " + e.zip + "\nMobileNumber: " + e.phoneNumber);
             }
         }
+
+        /// <summary>
+        /// UC 23 Add Multiple Address
+        /// </summary>
+        [TestMethod]
+        public void AddMultiple_Address_BY_API()
+        {
+            List<addressWork> addMultiple = new List<addressWork>();
+            addMultiple.Add(new addressWork { firstName = "Ahtesham",lastName = "Ansari",address = "Dighi",city = "Pune",state = "Maha",zip = "413512",phoneNumber = "816372829",BookName="Friend",BookType="Friend",Date = new DateTime(2020, 01, 01) });
+            addMultiple.Add(new addressWork { firstName = "Mohsin", lastName = "Ansari", address = "Dighi", city = "Pune", state = "Maha", zip = "413512", phoneNumber = "816372829", BookName = "Friend", BookType = "Friend", Date = new DateTime(2020, 01, 01) });
+            addMultiple.Add(new addressWork { firstName = "Nijam", lastName = "Ansari", address = "Dighi", city = "Pune", state = "Maha", zip = "413512", phoneNumber = "816372829", BookName = "Friend", BookType = "Friend", Date = new DateTime(2020, 01, 01) });
+            addMultiple.ForEach(record =>
+            {
+                    RestRequest request = new RestRequest("/AddressBook", Method.POST);
+                    JObject jObjectBody = new JObject();
+                    jObjectBody.Add("firstName", record.firstName);
+                    jObjectBody.Add("lastName", record.lastName);
+                    jObjectBody.Add("address", record.address);
+                    jObjectBody.Add("city", record.city);
+                    jObjectBody.Add("state", record.state);
+                    jObjectBody.Add("zip", record.zip);
+                    jObjectBody.Add("phoneNumber", record.phoneNumber);
+                    jObjectBody.Add("BookName", record.BookName);
+                    jObjectBody.Add("BookType", record.BookType);
+                    jObjectBody.Add("Date", record.Date);
+        
+                    request.AddParameter("application/json", jObjectBody, ParameterType.RequestBody);
+                    IRestResponse response = client.Execute(request);
+                    Assert.AreEqual(response.StatusCode, HttpStatusCode.Created);
+                    addressWork dataResorce = JsonConvert.DeserializeObject<addressWork>(response.Content);
+
+                    Assert.AreEqual(record.firstName, dataResorce.firstName);
+                    Assert.AreEqual(record.lastName, dataResorce.lastName);
+                    Assert.AreEqual(record.address, dataResorce.address);
+                    Assert.AreEqual(record.city, dataResorce.city);
+                    Assert.AreEqual(record.state, dataResorce.state);
+                    Assert.AreEqual(record.zip, dataResorce.zip);
+                    Assert.AreEqual(record.phoneNumber, dataResorce.phoneNumber);
+                    Assert.AreEqual(record.BookName, dataResorce.BookName);
+                    Assert.AreEqual(record.BookType, dataResorce.BookType);
+                    Assert.AreEqual(record.Date, dataResorce.Date);
+                    Console.WriteLine(response.Content);
+            });
+        }
     }
 }
